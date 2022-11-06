cfcb613b09062fa7091219fc800e0b546bfa4519
gasparESB <vitorgaspar.aog@gmail.com>
2022-11-06 20:24:18 -0300

    feat[pages]: create register page

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,12 @@
+import Form from "../../components/Form";
+import styles from "./styles.module.scss"
+
+const Register = () => {
+  return (
+    <div className={styles.registerWrapper}>
+      <Form />
+    </div>
+  );
+};
+
+export default Register;
