c806bcba17bbc703e3e5b0fdc20595e25a4a0f76
gasparESB <vitorgaspar.aog@gmail.com>
2022-11-06 20:41:46 -0300

    refactor[app]: add register component to render in main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-
+import Register from "./pages/Register";
+import "./styles/global.scss";
 
 function App() {
-
   return (
     <>
+      <Register />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
