37ea08b0e58e33f2e52affc20b438e24e61b409a
gasparESB <vitorgaspar.aog@gmail.com>
2022-11-06 20:46:00 -0300

    style[input]: create style archive to input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,10 +1,18 @@
-import React, { InputHTMLAttributes } from "react"
+import React, { InputHTMLAttributes } from "react";
+import { Path, UseFormRegister } from "react-hook-form";
+import styles from './styles.module.scss'
 
-
-const Input = ({...otherProps}: InputHTMLAttributes<HTMLInputElement>) => {
-    return (<>
-    <input {...otherProps} />
-    </>)
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: Path<any>;
+  register: UseFormRegister<any>;
+  required?: boolean;
 }
+const Input = ({label, register, ...otherProps }: InputProps) => {
+  return (
+    <>
+      <input className={styles.input} {...register(label)} {...otherProps} />
+    </>
+  );
+};
 
-export default Input
+export default Input;
