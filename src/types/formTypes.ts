7afebdba93d7638d288d492c8169de3e92f0d295
gasparESB <vitorgaspar.aog@gmail.com>
2022-11-07 17:52:28 -0300

    refactior[types]: add key type mapping

--- src/types/formTypes.ts
+++ src/types/formTypes.ts
@@ -1,15 +1,16 @@
 export type Adress = {
-    uf: string;
-    bairro: string;
-    localidade: string;
-  };
-  
-  export type FormType = {
-    nome: string;
-    cep: number;
-    email: string;
-    senha: string;
-    bairro: string;
-    estado: string;
-    cidade: string;
-  };
+  [key: string]: string;
+  uf: string;
+  bairro: string;
+  localidade: string;
+};
+
+export type FormType = {
+  nome: string;
+  cep: number;
+  email: string;
+  senha: string;
+  bairro: string;
+  estado: string;
+  cidade: string;
+};
